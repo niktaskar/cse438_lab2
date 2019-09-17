cec77464775c81f23a1777080f88f956556b81da
Nikash Taskar <nikashtaskar@wustl.edu>
2019-09-17 17:31:35 -0500

    Layout work done, adding functionality

---
+++ Animals.swift
@@ -0,0 +1,27 @@
+//
+//  Animals.swift
+//  lab2
+//
+//  Created by Nikash Taskar on 9/17/19.
+//  Copyright © 2019 Nikash Taskar. All rights reserved.
+//
+
+import Foundation
+
+class Animals {
+    
+    enum type {
+        case Dog
+        case Cat
+        case Bird
+        case Bunny
+        case Fish
+    }
+    
+    var foodLevel:Int = 0
+    var happiness:Int = 0
+    
+    func feed(){
+        
+    }
+}
